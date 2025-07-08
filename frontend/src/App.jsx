7e74a3f83edeab810d160d55cb563f21b2bf283d
Fabiha Chowdhury Momo <momo@Fabihas-MacBook-Air.local>
2025-07-08 20:06:07 +0600

    testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,8 @@ import { Outlet } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div >
-      <h1>Shahjalal</h1>
+    <div>
+      <h1>ishika</h1>
       <main className='min-h-screen max-w-7xl mx-auto px-4 py-6'>
       <Outlet/>
       </main>
