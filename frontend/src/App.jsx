cdf4dc0ef2263d1e85d27ea27d49728d5ac07c08
MostakemHossain <mostakememon123@gmail.com>
2025-07-08 18:55:41 +0600

    react-router add and font customization

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className='text-5xl'>App</div>
+    <div >
+      <h1>Hello</h1>
+      <main className='min-h-screen max-w-7xl mx-auto px-4 py-6'>
+      <Outlet/>
+      </main>
+      <h1>Hello</h1>
+    </div>
   )
 }
 
