a536baee08f00ba66ae2ac7571102f8ed16b32a3
MostakemHossain <mostakememon123@gmail.com>
2025-07-14 19:55:59 +0600

    login and signup route setup

--- frontend/src/routes/router.jsx
+++ frontend/src/routes/router.jsx
@@ -1,20 +1,30 @@
 import App from "@/App";
 import Home from "@/pages/home/Home";
+import Login from "@/pages/login";
+import SignUp from "@/pages/SignUp";
 import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
-    children:[
-        {
-            path:"/",
-            element: <Home/>,
-        }
-    ]
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
   {
-    path: "*", 
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/sign-up",
+    element: <SignUp />,
+  },
+  {
+    path: "*",
     element: <div>404 - Page Not Found</div>,
   },
 ]);
